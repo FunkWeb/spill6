828f0479a7c5eadbd25028a9db82b3d5f8b79041
Jørgen <70204936+NorskViking@users.noreply.github.com>
2024-02-09 08:44:58 +0100

    added scene change to test_level
    Added a short wait timer and scene change to the test_level, so that you are returned to the main screen on win or death.

--- scenes/test_level.gd
+++ scenes/test_level.gd
@@ -9,7 +9,11 @@ func _on_death_pit_detected(_self, body):
 func _on_goal_detected(_self, body):
 	body.queue_free()
 	$HUD/CenterContainer/you_win.show()
+	await get_tree().create_timer(5).timeout
+	get_tree().change_scene_to_file("res://scenes/menu.tscn")
 
 func _on_player_death(body):
 	body.queue_free()
 	$HUD/CenterContainer/you_died.show()
+	await get_tree().create_timer(5).timeout
+	get_tree().change_scene_to_file("res://scenes/menu.tscn")
