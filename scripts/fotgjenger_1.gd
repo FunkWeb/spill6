1220f3d969eacd5bb65540e2d91dca51850fc8b4
Jørgen <70204936+NorskViking@users.noreply.github.com>
2024-02-19 14:56:58 +0100

    small bugfixes
    Fixed some smaller bugs, like unused variables and use of variable names that was similar to existing variable names.

--- scripts/fotgjenger_1.gd
+++ scripts/fotgjenger_1.gd
@@ -6,8 +6,8 @@ func _ready():
 	print("fotgjenger 1 loaded")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	# Update animation
+func _process(_delta):
+	#Update animation
 	pass
 	
 func _on_area_2d_area_entered(body):
--- scripts/game_timer.gd
+++ scripts/game_timer.gd
@@ -19,7 +19,7 @@ func _ready():
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _process(_delta):
 	# Set the label text to the current time left on timer
 	label.text = get_time()	
 
@@ -30,21 +30,21 @@ func get_time():
 	var secounds:int = fmod(current_time, 60.0)
 	var msec:int = fmod(current_time, 1) * 100
 	# Convert the integers into string format
-	var min = str(minutes)
-	var sec = str(secounds)
+	var mins = str(minutes)
+	var secs = str(secounds)
 	var ms = str(msec)
 	# Format the string to a better visiual reprensentation
 	if minutes < 10:
-		min = "0" + str(minutes)
+		mins = "0" + str(minutes)
 	if secounds < 10:
-		sec = "0" + str(secounds)
+		secs = "0" + str(secounds)
 	if msec < 10:
 		if msec == 0:
 			ms = "00"
 		else:
 			ms = "0" + str(msec)
 	# Return the converted and formated time in a 00:00:00 format
-	return str(min) + ":" + str(sec) + ":" + str(ms)
+	return str(mins) + ":" + str(secs) + ":" + str(ms)
 
 func _on_timer_timeout():
 	# Send signal and transition to GameOver/Retry menu
