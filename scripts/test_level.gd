ce414fae011cd764dc794d718c3d541bcb1991a7
Tormod-Rasmussen <tormodrasmussen@gmail.com>
2024-02-19 14:21:08 +0100

    skjuler hud når man pauser

--- scripts/test_level.gd
+++ scripts/test_level.gd
@@ -1,6 +1,7 @@
 extends Node2D
 @onready var pause = %Pause
 @onready var camera = $Camera2D
+@onready var hud = $HUD
 # Viser label og "dreper" om player har sendt ut death signalet
 # body.queue_free() "sletter" spilleren 
 
@@ -18,9 +19,11 @@ func _notification(what):
 	or what == NOTIFICATION_WM_CLOSE_REQUEST:
 		# windows close button / android home button / android back button
 		if pause.visible == true:
+			hud.show()
 			camera.enabled = true
 			pause._on_resume_button_pressed()
 		else:
 			pause.show()
+			hud.hide()
 			camera.enabled = false
 			get_tree().paused = true
