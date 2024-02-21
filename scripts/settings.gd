16b2196e6a9521f658c02d47db1e4fce645225bd
Tormod-Rasmussen <tormodrasmussen@gmail.com>
2024-02-21 14:08:20 +0100

    fikset volum slidere beholder posisjon
    volum slidere henter nå riktig posisjon fra audiobus når den åpnes

--- scripts/settings.gd
+++ scripts/settings.gd
@@ -4,8 +4,17 @@ extends Control
 # slidere for musikk of lydeffekter
 # annet??
 
-func _ready():
-	print("settings loaded")
+func _on_close_button_pressed():
+	$".".hide()
+
+func _on_music_slider_value_changed(value):
+	SoundManager.adjust_music_volume(value)
+
+func _on_sound_effect_slider_value_changed(value):
+	SoundManager.adjust_sound_effect_volume(value)
+
+
+func _on_visibility_changed():
 	# henter lydnivå fra audio bus så sliderene viser riktig verdi
 	$MusicSlider.value = db_to_linear(
 		AudioServer.get_bus_volume_db(
@@ -17,12 +26,3 @@ func _ready():
 			AudioServer.get_bus_index("sfx")
 		)
 	)
-
-func _on_close_button_pressed():
-	$".".hide()
-
-func _on_music_slider_value_changed(value):
-	SoundManager.adjust_music_volume(value)
-
-func _on_sound_effect_slider_value_changed(value):
-	SoundManager.adjust_sound_effect_volume(value)
