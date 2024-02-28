a89c9aa0975cbbef5e31aec0b04a2dd5c03081fc
JonathλnPR <jonathan.reppen@outlook.com>
2024-02-28 13:52:07 +0100

    ny w1m1 og spiller fiks

---
+++ scripts/level/gameover.gd
@@ -0,0 +1,11 @@
+extends Node2D
+@onready var camera = $Camera2D
+@onready var hud = $HUD
+# Viser label og "dreper" om player har sendt ut death signalet
+# body.queue_free() "sletter" spilleren 
+
+func _on_player_death(body):
+	body.queue_free()
+	$HUD/CenterContainer/YouDiedLabel.show()
+	await get_tree().create_timer(2).timeout
+	get_tree().change_scene_to_file("res://scenes/menus/main_menu.tscn")
--- scripts/ui/health.gd
+++ scripts/ui/health.gd
@@ -32,3 +32,11 @@ func _on_health_upgrade_detected(amount):
 	for i in range(amount):
 		text += health_icon
 	print('increasing health')
+
+
+func on_hit(from, to):
+	pass # Replace with function body.
+
+
+func _on_boar_03_hit(from, to):
+	pass # Replace with function body.
