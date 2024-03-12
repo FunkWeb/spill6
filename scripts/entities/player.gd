639cee531a368f5e24a6821ba0bdb9f53b53e90c
Taxato <taxatoftw@gmail.com>
2024-03-12 12:25:20 +0100

    Spiller animasjoner
    lagt til animasjoner for skiking og hopping

--- scripts/entities/player.gd
+++ scripts/entities/player.gd
@@ -1,6 +1,7 @@
 extends CharacterBody2D
 
 signal has_died(body)
+signal lost_health(body)
 # TODO
 # Legg til relevant bevegelse
 # Ask if doublejump is something we should add
@@ -8,15 +9,15 @@ signal has_died(body)
 
 @export var speed : float = 300.0
 @export var jump_velocity: float = -450.0
-@export var double_jump_velocity: float = - 350.0
 @export var scroll_speed : float = 0
 
-@onready var anim_player : AnimationPlayer = $AnimationPlayer
-
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var has_double_jumped : bool = false
 var has_landed : bool = false
 var animation_locked : bool = false
 var hearts : int = 3
+
+@onready var anim_sprite = $"Sprites/AnimatedSprite2D"
 
 func _ready():
 	print("player loaded")
@@ -25,30 +26,27 @@ func _physics_process(delta):
 	position.x += scroll_speed * delta
 	if not is_on_floor():
 		velocity.y += gravity * delta
+	if not anim_sprite.is_playing() and is_on_floor():
+		anim_sprite.play("skike")
+
 	has_landed = check_if_landing()
-	update_animation()
 	move_and_slide()
 
-func update_animation():
-	if is_on_floor():
-		anim_player.play('run')
-	else:
-		anim_player.play('float')
-	if has_landed:
-		# Funker ikke 
-		anim_player.play('jump_land')
 
 func check_if_landing():
 	return (not is_on_floor() and velocity.y > 50)
-
-func on_hit(entity, body):	
+	
+	
+func _on_hit(entity, body):
 	print('player hit')
+	emit_signal("lost_health")
 	hearts -= 1
 	# play hit animation
 	# push character back a bit?
 	# invincibility, flashing sprite?
 	if hearts == 0:
 		die(body)
+
 
 func die(body):
 	print('player has died')
@@ -59,7 +57,8 @@ func _on_health_upgrade_detected(amount):
 	print('player received health upgrade')
 	hearts += amount
 
-func _on_jump_button_jump():
+func _on_jump_button_pressed():
 	if is_on_floor():
-		anim_player.play('jump_start')
+		has_double_jumped = false
+		anim_sprite.play("jump")
 		velocity.y = jump_velocity
