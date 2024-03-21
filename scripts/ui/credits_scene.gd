662a4be96ffee3d8875ae6e51e3bf1dc35d2a088
Thomas <taxatoftw@gmail.com>
2024-03-21 15:17:34 +0100

    Fikset kapitalisering
    Filnavn var skrevet med feil store bokstaver

--- scripts/ui/credits_scene.gd
+++ scripts/ui/credits_scene.gd
@@ -1,7 +1,7 @@
 extends Control
 
 func _ready():
-	var file = FileAccess.open("res://CREDITS.MD",FileAccess.READ)
+	var file = FileAccess.open("res://CREDITS.md",FileAccess.READ)
 	var text = file.get_as_text(true)
 	$Text.markdown_text = "[center]\n"+text
 
